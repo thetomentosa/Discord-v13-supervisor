f57f51bc86c708311d51206d129bde43abcb5216
TheTomenTosa <58092666+thetomentosa@users.noreply.github.com>
2022-04-23 13:03:41 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+const { Client, Collection, Intents } = require("discord.js");
+const client = global.client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_VOICE_STATES,
+    Intents.FLAGS.GUILD_PRESENCES,
+  ]
+});
+const dotenv = require("dotenv");
+dotenv.config();
+const { readdir } = require("fs");
+require("moment-duration-format");
+const commands = client.commands = new Collection();
+const aliases = client.aliases = new Collection();
+client.cooldown = new Map();
+client.commandblocked = [];
+
+require("./src/helpers/function")(client);
+
+readdir("./src/commands/", (err, files) => {
+  if (err) console.error(err)
+  files.forEach(f => {
+    readdir("./src/commands/" + f, (err2, files2) => {
+      if (err2) console.log(err2)
+      files2.forEach(file => {
+        let prop = require(`./src/commands/${f}/` + file);
+        console.log(`[TheTomenTosa Dev.- Komutlar] ${prop.name} yüklendi!`);
+        commands.set(prop.name, prop);
+        prop.aliases.forEach(alias => {
+          aliases.set(alias, prop.name);
+        });
+      });
+    });
+  });
+});
+
+readdir("./src/events", (err, files) => {
+  if (err) return console.error(err);
+  files.filter((file) => file.endsWith(".js")).forEach((file) => {
+    let prop = require(`./src/events/${file}`);
+    if (!prop.conf) return;
+    client.on(prop.conf.name, prop);
+    console.log(`[TheTomenTosa Dev. - Event] ${prop.conf.name} yüklendi!`);
+  });
+});
+
+client.login(process.env.TOKEN)
+  .then(() => console.log(`Bot ${client.user.username} olarak giriş yaptı!`))
+  .catch((err) => console.log(`Bot Giriş yapamadı sebep: ${err}`));
